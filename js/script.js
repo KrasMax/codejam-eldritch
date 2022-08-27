e7c85e1873fd150abc7fa379e8c0fc4b8cef6037
KrasMax <maximkraskovskiy@gmail.com>
2022-08-27 18:00:49 +0300

    complited level easy hard and average

---
+++ js/script.js
@@ -0,0 +1,735 @@
+const cardsDataBlue = [
+    {
+      id: 'blue1',
+      difficulty: 'hard',
+      color:'blue'
+    },
+    {
+      id: 'blue2',
+      difficulty: 'hard',
+      color:'blue'
+    },
+    {
+      id: 'blue3',
+      difficulty: 'easy',
+      color:'blue'
+    },
+    {
+      id: 'blue4',
+      difficulty: 'easy',
+      color:'blue'
+    },
+    {
+      id: 'blue5',
+      difficulty: 'easy',
+      color:'blue'
+    },
+    {
+      id: 'blue6',
+      difficulty: 'hard',
+      color:'blue'
+    },
+    {
+      id: 'blue7',
+      difficulty: 'normal',
+      color:'blue'
+    },
+    {
+      id: 'blue8',
+      difficulty: 'hard',
+      color:'blue'
+    },
+    {
+      id: 'blue9',
+      difficulty: 'normal',
+      color:'blue'
+    },
+    {
+      id: 'blue10',
+      difficulty: 'easy',
+      color:'blue'
+    },
+    {
+      id: 'blue11',
+      difficulty: 'normal',
+      color:'blue'
+    },
+    {
+      id: 'blue12',
+      difficulty: 'normal',
+      color:'blue'
+    },
+  ]
+
+const cardsDataBrown = [
+    {
+      id: 'brown1',
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown2',
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown3',
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown4',
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown5',
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown6',
+      difficulty: 'hard',
+      color:'brown'
+    },
+    {
+      id: 'brown7',
+      difficulty: 'hard',
+      color:'brown'
+    },
+    {
+      id: 'brown8',
+      difficulty: 'hard',
+      color:'brown'
+    },
+    {
+      id: 'brown9',
+      difficulty: 'hard',
+      color:'brown'
+    },
+    {
+      id: 'brown10',
+      difficulty: 'hard',
+      color:'brown'
+    },
+    {
+      id: 'brown11',
+      difficulty: 'easy',
+      color:'brown'
+    },
+    {
+      id: 'brown12',
+      difficulty: 'easy',
+      color:'brown'
+    },
+    {
+      id: 'brown13',
+      difficulty: 'easy',
+      color:'brown'
+    },
+    {
+      id: 'brown14',
+      difficulty: 'easy',
+      color:'brown'
+    },
+    {
+      id: 'brown15',
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown16',
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown17',
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown18',
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown19',
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown20',
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown21',
+      difficulty: 'easy',
+      color:'brown'
+    },
+  ]
+
+const cardsDataGreen = [
+    {
+      id: 'green1',
+      difficulty: 'easy',
+      color:'green'
+    },
+    {
+      id: 'green2',
+      difficulty: 'hard',
+      color:'green'
+    },
+    {
+      id: 'green3',
+      difficulty: 'hard',
+      color:'green'
+    },
+    {
+      id: 'green4',
+      difficulty: 'hard',
+      color:'green'
+    },
+    {
+      id: 'green5',
+      difficulty: 'hard',
+      color:'green'
+    },
+    {
+      id: 'green6',
+      difficulty: 'hard',
+      color:'green'
+    },
+    {
+      id: 'green7',
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green8',
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green9',
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green10',
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green11',
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green12',
+      difficulty: 'easy',
+      color:'green'
+    },
+    {
+      id: 'green13',
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green14',
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green15',
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green16',
+      difficulty: 'easy',
+      color:'green'
+    },
+    {
+      id: 'green17',
+      difficulty: 'easy',
+      color:'green'
+    },
+    {
+      id: 'green18',
+      difficulty: 'easy',
+      color:'green'
+    },
+  ]
+
+const ancientsData = [
+    {
+      id: 'azathoth',
+      name: 'azathoth',
+      firstStage: {
+        greenCards: 1,
+        blueCards: 1,
+        brownCards: 2,
+      },
+      secondStage: {
+        greenCards: 2,
+        blueCards: 1,
+        brownCards: 3,
+      },
+      thirdStage: {
+        greenCards: 2,
+        blueCards: 0,
+        brownCards: 4,
+      },
+    },
+    {
+      id: 'cthulhu',
+      name: 'cthulhu',
+      firstStage: {
+        greenCards: 0,
+        blueCards: 2,
+        brownCards: 2,
+      },
+      secondStage: {
+        greenCards: 1,
+        blueCards: 0,
+        brownCards: 3,
+      },
+      thirdStage: {
+        greenCards: 3,
+        blueCards: 0,
+        brownCards: 4,
+      },
+    },
+    {
+      id: 'iog_sothoth',
+      name: 'iog_sothoth',
+      firstStage: {
+        greenCards: 0,
+        blueCards: 1,
+        brownCards: 2,
+      },
+      secondStage: {
+        greenCards: 2,
+        blueCards: 1,
+        brownCards: 3,
+      },
+      thirdStage: {
+        greenCards: 3,
+        blueCards: 0,
+        brownCards: 4,
+      },
+    },
+    {
+      id: 'shub_niggurath',
+      name: 'shub_niggurath',
+      firstStage: {
+        greenCards: 1,
+        blueCards: 1,
+        brownCards: 2,
+      },
+      secondStage: {
+        greenCards: 3,
+        blueCards: 1,
+        brownCards: 2,
+      },
+      thirdStage: {
+        greenCards: 2,
+        blueCards: 0,
+        brownCards: 4,
+      },
+    },
+  ]
+
+const difficulties = [
+    {
+        id: 'easy',
+        name: 'Низкая'
+    },
+    {
+        id: 'normal',
+        name: 'Средняя'
+    },
+    {
+        id: 'hard',
+        name: 'Высокая'
+    },
+  ]
+
+// большие карты
+const azathoth = document.querySelector('.azathoth');
+const cthulthu = document.querySelector('.cthulhu');
+const logSothoth = document.querySelector('.log_sothoth');
+const shubNiggurath = document.querySelector('.shub_niggurath')
+
+
+// кнопки уровней
+const boxLevelSelection = document.querySelector('.wrap_box_level_selection')
+const btnLevelVeryLight = document.querySelector('#very-light');
+const btnLevelLight = document.querySelector('#light');
+const btnLevelAverage = document.querySelector('#average');
+const btnLevelHigh = document.querySelector('#high');
+const btnLevelVeryTall = document.querySelector('#very-tall');
+
+
+// старт игры
+const startGame = document.querySelector('.wraper_btn_start_game');
+const btnStartGame = document.querySelector('.position_btn_start');
+const playingField = document.querySelector('.wrap_playing_field');
+const boxLevelCounter = document.querySelector('.box_level_counter');
+const boxMythicCards = document.querySelector('.box_mythic_cards');
+
+// количество карт по цветам
+let numberGreenCards = 0;
+let numberBlueCards = 0;
+let numberBrownCards = 0;
+
+// подколоды по цветам
+let startGreenCard;
+let startBlueCard;
+let startBrownCard;
+
+// колоды по уровням
+let stageOne = [];
+let stageTwo = [];
+let stageThree = [];
+
+
+let activeAncients
+
+const btnNextCard = document.querySelector('.shirt')
+const playingСardАield = document.querySelector('.box_activ_mythic_cards')
+
+
+const clearActiv = () => {
+  let colectionItems = document.querySelectorAll('.big_map')
+  colectionItems.forEach((elem) => {
+    elem.classList.remove('active_big_map');
+  })
+}
+
+const selectCard = (arr, amount) => {
+  const selectData = []
+  do {
+      arrayRandElement(arr)
+  } while (selectData.length != amount);
+  function arrayRandElement(arr) {
+      let rand = Math.floor(Math.random() * arr.length);
+      selectData.push(arr[rand])
+  }
+  return selectData      
+}
+
+const runBigMap = (obj) => {
+  numberGreenCards = obj.firstStage.greenCards + obj.secondStage.greenCards + obj.thirdStage.greenCards
+  numberBlueCards = obj.firstStage.blueCards + obj.secondStage.blueCards + obj.thirdStage.blueCards
+  numberBrownCards = obj.firstStage.brownCards + obj.secondStage.brownCards + obj.thirdStage.brownCards
+}
+
+
+const collectCardsByLevels = (obj) => {
+
+  let green1 = startGreenCard.splice(0, obj.firstStage.greenCards);
+  let blue1 = startBlueCard.splice(0, obj.firstStage.blueCards);
+  let brown1 = startBrownCard.splice(0, obj.firstStage.brownCards);
+
+  let sort1 = green1.concat(blue1 , brown1)
+  stageOne = sort1.sort(function(){ return 0.5-Math.random() })
+
+  let green2 = startGreenCard.splice(0, obj.secondStage.greenCards);
+  let blue2 = startBlueCard.splice(0, obj.secondStage.blueCards);
+  let brown2 = startBrownCard.splice(0, obj.secondStage.brownCards);
+
+  let sort2 = green2.concat(blue2, brown2)
+  stageTwo = sort2.sort(function(){ return 0.5-Math.random() })
+
+  let green3 = startGreenCard.splice(0, obj.thirdStage.greenCards);
+  let blue3 = startBlueCard.splice(0, obj.thirdStage.blueCards);
+  let brown3 = startBrownCard.splice(0, obj.thirdStage.brownCards);
+  
+
+  let sort3 = green3.concat(blue3, brown3)
+  stageThree = sort3.sort(function(){ return 0.5-Math.random() })
+  
+}
+
+const spreadOutTheDeck = () => {
+
+
+  // поля остатка карт
+    const greenOne = document.querySelector('#green-one');
+    const brownOne = document.querySelector('#brown-one');
+    const blueOne = document.querySelector('#blue-one');
+
+    const grinTwo = document.querySelector('#grin-two');
+    const brownTwo = document.querySelector('#brown-two');
+    const blueTwo = document.querySelector("#blue-two");
+
+    const greenThree = document.querySelector('#green-three');
+    const brownThree = document.querySelector('#brown-three');
+    const blueThree = document.querySelector('#blue-three');
+
+  let urlImg = ''
+
+  if(stageOne.length != 0){
+    let curentCard = stageOne.pop(stageOne[0])
+
+    if(stageOne.length === 0){
+      const endstageOne = document.querySelector('#endstage-one')
+      endstageOne.classList.add('endStage') 
+    }
+
+          if(curentCard.color === "blue"){
+            let val = blueOne.textContent
+            blueOne.textContent = `${parseInt(val) - 1}`
+          }else if(curentCard.color === "green"){
+            let val = greenOne.textContent
+            greenOne.textContent = `${parseInt(val) - 1}`
+          }else if (curentCard.color === "brown"){
+            let val = brownOne.textContent
+            brownOne.textContent = `${parseInt(val) - 1}`
+          }
+
+      if (curentCard.color === 'blue'){
+        urlImg = `url(../img/MythicCards/blue/${curentCard.id}.png)`
+
+      }else if (curentCard.color === 'green'){
+        urlImg = `url(../img/MythicCards/green/${curentCard.id}.png)`
+
+      }else if (curentCard.color === 'brown'){
+        urlImg = `url(../img/MythicCards/brown/${curentCard.id}.png)`
+
+      }
+
+      playingСardАield.style.backgroundImage = urlImg
+
+  }else if (stageTwo.length != 0){
+
+    let curentCard = stageTwo.pop(stageTwo[0])
+
+      if (stageTwo.length === 0){
+         const endstageTwo = document.querySelector('#endstage-two');
+
+         endstageTwo.parentElement.classList.remove('number_level')
+         endstageTwo.parentElement.classList.add('endStage2')
+      }
+
+        if(curentCard.color === "blue"){
+          let val = blueTwo.textContent
+          blueTwo.textContent = `${parseInt(val) - 1}`
+        }else if(curentCard.color === "green"){
+          let val = grinTwo.textContent
+          grinTwo.textContent = `${parseInt(val) - 1}`
+        }else if (curentCard.color === "brown"){
+          let val = brownTwo.textContent
+          brownTwo.textContent = `${parseInt(val) - 1}`
+        }
+
+
+      if (curentCard.color === 'blue'){
+        urlImg = `url(../img/MythicCards/blue/${curentCard.id}.png)`
+        
+      }else if (curentCard.color === 'green'){
+        urlImg = `url(../img/MythicCards/green/${curentCard.id}.png)`
+        
+      }else if (curentCard.color === 'brown'){
+        urlImg = `url(../img/MythicCards/brown/${curentCard.id}.png)`
+        
+      }
+      playingСardАield.style.backgroundImage = urlImg
+
+  }else if (stageThree.length != 0){
+
+    let curentCard = stageThree.pop(stageThree[0])
+
+      if (stageThree.length === 0){
+        btnNextCard.style.backgroundImage = 'url()'
+        const endstageThree = document.querySelector('#endstage-three')
+        endstageThree.classList.add('endStage') 
+      }
+
+
+        if(curentCard.color === "blue"){
+          let val = blueThree.textContent
+          blueThree.textContent = `${parseInt(val) - 1}`
+        }else if(curentCard.color === "green"){
+          let val = greenThree.textContent
+          greenThree.textContent = `${parseInt(val) - 1}`
+        }else if (curentCard.color === "brown"){
+          let val = brownThree.textContent
+          brownThree.textContent = `${parseInt(val) - 1}`
+        }
+
+      if (curentCard.color === 'blue'){
+        urlImg = `url(../img/MythicCards/blue/${curentCard.id}.png)`
+        
+      }else if (curentCard.color === 'green'){
+        urlImg = `url(../img/MythicCards/green/${curentCard.id}.png)`
+        
+      }else if (curentCard.color === 'brown'){
+        urlImg = `url(../img/MythicCards/brown/${curentCard.id}.png)`
+        
+      }
+      playingСardАield.style.backgroundImage = urlImg
+    }
+
+}
+
+const readCunterCard = (obj) => {
+
+    const greenOne = document.querySelector('#green-one');
+    const brownOne = document.querySelector('#brown-one');
+    const blueOne = document.querySelector('#blue-one');
+
+    const grinTwo = document.querySelector('#grin-two');
+    const brownTwo = document.querySelector('#brown-two');
+    const blueTwo = document.querySelector("#blue-two");
+
+    const greenThree = document.querySelector('#green-three');
+    const brownThree = document.querySelector('#brown-three');
+    const blueThree = document.querySelector('#blue-three');
+
+    greenOne.textContent = `${obj.firstStage.greenCards}`
+    brownOne.textContent = `${obj.firstStage.brownCards}`
+    blueOne.textContent = `${obj.firstStage.blueCards}`
+
+    grinTwo.textContent = `${obj.secondStage.greenCards}`
+    brownTwo.textContent = `${obj.secondStage.brownCards}`
+    blueTwo.textContent = `${obj.secondStage.blueCards}`
+
+    greenThree.textContent = `${obj.thirdStage.greenCards}`
+    brownThree.textContent = `${obj.thirdStage.brownCards}`
+    blueThree.textContent = `${obj.thirdStage.blueCards}`
+
+}
+
+const choiceOfDifficultyLevel = (selectStr) => {
+
+  cardsDataBlue.forEach((elem, index) => {
+      if(elem.difficulty === selectStr){
+        cardsDataBlue.splice(index, 1);
+      }
+  })
+
+  cardsDataBlue.forEach((elem, index) => {
+    if(elem.difficulty === selectStr){
+      cardsDataBlue.splice(index, 1);
+    }
+  })
+
+  cardsDataBrown.forEach((elem, index) => {
+    if(elem.difficulty === selectStr){
+      cardsDataBrown.splice(index, 1);
+    }
+  })
+
+  cardsDataBrown.forEach((elem, index) => {
+    if(elem.difficulty === selectStr){
+      cardsDataBrown.splice(index, 1);
+    }
+  })
+
+  cardsDataGreen.forEach((elem, index) => {
+    if(elem.difficulty === selectStr){
+      cardsDataGreen.splice(index, 1);
+    }
+  })
+
+  cardsDataGreen.forEach((elem, index) => {
+    if(elem.difficulty === selectStr){
+      cardsDataGreen.splice(index, 1);
+    }
+  })
+}
+
+window.addEventListener('click', (event) => {
+
+    if(event.target.classList.contains('big_map')){
+        boxLevelSelection.classList.remove('no_show')
+        clearActiv()
+        event.target.classList.add('active_big_map')
+          if(event.target.classList.contains('azathoth')){
+            activeAncients = ancientsData[0]
+            runBigMap(ancientsData[0]) 
+          }
+          if(event.target.classList.contains('cthulhu')){
+            activeAncients = ancientsData[1]
+            runBigMap(ancientsData[1]) 
+          }
+          if(event.target.classList.contains('log_sothoth')){
+            activeAncients = ancientsData[2]
+            runBigMap(ancientsData[2]) 
+          }
+          if(event.target.classList.contains('shub_niggurath')){
+            activeAncients = ancientsData[3]
+            runBigMap(ancientsData[3]) 
+          }
+    }
+
+    if(event.target === btnLevelAverage){
+        startGame.classList.remove('no_show')
+        btnLevelAverage.classList.add('active_level')
+
+        startGreenCard = selectCard(cardsDataGreen, numberGreenCards);
+        startBlueCard = selectCard(cardsDataBlue, numberBlueCards);
+        startBrownCard = selectCard(cardsDataBrown, numberBrownCards)
+
+    }else if(event.target === btnLevelLight){
+        choiceOfDifficultyLevel("hard")
+        startGame.classList.remove('no_show')
+        btnLevelLight.classList.add('active_level')
+
+        startGreenCard = selectCard(cardsDataGreen, numberGreenCards);
+        startBlueCard = selectCard(cardsDataBlue, numberBlueCards);
+        startBrownCard = selectCard(cardsDataBrown, numberBrownCards)
+
+    }else if (event.target === btnLevelHigh){
+        choiceOfDifficultyLevel("easy")
+        startGame.classList.remove('no_show')
+        btnLevelHigh.classList.add('active_level')
+
+        startGreenCard = selectCard(cardsDataGreen, numberGreenCards);
+        startBlueCard = selectCard(cardsDataBlue, numberBlueCards);
+        startBrownCard = selectCard(cardsDataBrown, numberBrownCards)
+
+    }
+
+
+    if(event.target === btnStartGame){
+        startGame.classList.add('no_show')
+        playingField.classList.remove('no_show')
+        collectCardsByLevels(activeAncients)
+        readCunterCard(activeAncients)
+
+        if(event.target.classList.contains('azathoth')){
+          activeAncients = ancientsData[0]
+        }
+        if(event.target.classList.contains('cthulhu')){
+          activeAncients = ancientsData[1]
+        }
+        if(event.target.classList.contains('log_sothoth')){
+          activeAncients = ancientsData[2]
+        }
+        if(event.target.classList.contains('shub_niggurath')){
+          activeAncients = ancientsData[3]  
+        }
+    }
+})
+
+window.addEventListener('click', (e) => {
+  if (e.target == btnNextCard){
+    spreadOutTheDeck()
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
